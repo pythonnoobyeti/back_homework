d1223426027913c47cdacb3f3d183fcb3bb2c10b
Alex Filenko <a.filenko@onpoint.ru>
2021-08-07 22:19:16 +0300

    Homework 3

---
+++ homework_3/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const { sayHiLev } = require("./my_module");
+
+console.log(sayHiLev());
---
+++ homework_3/my_module.js
@@ -0,0 +1,7 @@
+function sayHiLev() {
+  return "Hi, Lev!";
+}
+
+module.exports = {
+  sayHiLev,
+};
